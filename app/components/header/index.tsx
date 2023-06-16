b2bee2ded1ccdc679bec6878379261610b7c5b92
Matheus <matheus_dario@outlook.com>
2023-06-15 21:49:21 -0300

    feat: :sparkles: added header design

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,10 +1,24 @@
+'use client'
+
 import Image from "next/image"
 import Link from "next/link"
+import { NavItem } from "./nav-item"
+
+const NAV_ITEMS = [
+  {
+    label: 'Home',
+    href: '/'
+  },
+  {
+    label: 'Projetos',
+    href: '/projects'
+  }
+]
 
 export const Header = () => {
   return (
-    <header>
-      <div className="container">
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+      <div className="container flex items-center justify-between">
         <Link href="/">
           <Image 
             width={58}
@@ -13,6 +27,12 @@ export const Header = () => {
             alt="Logo"
           />
         </Link>
+
+        <nav className="flex items-center gap-4 sm:gap-10">
+          {NAV_ITEMS.map(item => (
+            <NavItem {...item} key={item.label} />
+          ))}
+        </nav>
       </div>
     </header>
   )
